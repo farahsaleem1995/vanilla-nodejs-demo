016cd4e4e9420e43abfd63b0e37ec7e651d2d87e
farahexa2020 <farahexa2020@gmail.com>
2020-05-24 15:38:53 +0300

    Sending response.

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const http = require("http");
 
 var server = http.createServer((req, res) => {
   console.log(req);
+
+  // Sending response
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>First Page</title></head>");
+  res.write("<body><h1>Hello from NodeJS Server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
