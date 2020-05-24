e695359dd3abf091327e9d51b9cbfa5459d74d4a
farahexa2020 <farahexa2020@gmail.com>
2020-05-24 16:49:13 +0300

    Routing requests.

--- app.js
+++ app.js
@@ -4,7 +4,25 @@ var server = http.createServer((req, res) => {
   console.log(req);
 
   // Sending response
+  const url = req.url;
   res.setHeader("Content-Type", "text/html");
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(`
+      <body>
+        <form method="POST" action="/message">
+          <input type="text" name="message">
+          <button type="submit">
+            Send
+          </button>
+        </form>
+      </body>`);
+    res.write("</html>");
+    return res.end();
+  }
+
   res.write("<html>");
   res.write("<head><title>First Page</title></head>");
   res.write("<body><h1>Hello from NodeJS Server</h1></body>");
