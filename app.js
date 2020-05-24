f673efe70f404b68170210a8b6699a252dab8bf5
farahexa2020 <farahexa2020@gmail.com>
2020-05-24 17:00:04 +0300

    Redirecting requests.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const http = require("http");
+const fs = require("fs");
 
 var server = http.createServer((req, res) => {
   console.log(req);
 
   // Sending response
   const url = req.url;
-  res.setHeader("Content-Type", "text/html");
+  const method = req.method;
 
   if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(`
@@ -23,6 +25,14 @@ var server = http.createServer((req, res) => {
     return res.end();
   }
 
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>First Page</title></head>");
   res.write("<body><h1>Hello from NodeJS Server</h1></body>");
